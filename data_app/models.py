c98c03597f2896f5d75385518518a1a0461383fc
Luka Konovalov <konovluka@gmail.com>
2025-04-08 16:37:58 +0300

    Got everything ready to build database using ORM.

--- data_app/models.py
+++ data_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+class History(models.Model):
+    name = models.TextField(max_length=75)
+
+    def __str__(self):
+        return f'{self.name}'
 class Person(models.Model):
+    foreign_key = models.ForeignKey(History, on_delete=models.CASCADE)
     name=models.TextField(max_length=75)
     trophy = models.IntegerField()
     date = models.TextField(max_length=75)
