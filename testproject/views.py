5472d3af938f8f81b445c4a61d8de2fb743a4a1a
antondep <antondep007@gmail.com>
2025-04-08 12:24:28 +0300

    Added better subredit showcase and average of comments

--- testproject/views.py
+++ testproject/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from reddit_request import DataGetter
-import random
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 def homepage(request):
     return render(request, "home.html")
@@ -19,7 +19,13 @@ def analysispage(request):
         query=query1
     data = d.get_user_analysis(query)
     up_v_down=0
+    up=0
     n=0
+    j=""
+    subredits1=''
+    subredits2=''
+    subredits3=""
+    comment_amount=0
     pic= DataGetter.get_reddit_avatar(query)
     date_today = datetime.today().strftime("%B %d %Y")
     date_of_creation =data.get('created_date', [])
@@ -34,11 +40,36 @@ def analysispage(request):
     post_karma=data.get('link_karma')
     comment = len(data.get('recent_comments', []))
     comment_karma=data.get('comment_karma')
-    subredits= [data.get('recent_posts', [])[0]['subreddit'],data.get('recent_posts', [])[1]['subreddit'],data.get('recent_posts', [])[3]['subreddit']]
+    subredits= []
     for i in data.get('recent_posts', []):
-        up_v_down +=i['score']
+        subredits.append(i['subreddit'])
+        up_v_down +=i['upvotes_ratio']
+        up+=i["score"]
+        comment_amount+=i['num_comments']
         n+=1
-
+    if len(subredits)>=3:
+        subredits1="r/"+subredits[0]
+        subredits2="r/"+subredits[1]
+        subredits3="r/"+subredits[2]
+        j=f"and {len(subredits)-3} more"
+    elif len(subredits)>=2:
+        subredits1="r/"+subredits[0]
+        subredits2="r/"+subredits[1]
+    else:
+        subredits1="r/"+subredits[0]
+    posts = data.get('recent_posts', [])
+    posts_amount = len(posts)
+    posting_frequency = 0
+    if posts:
+        try:
+            post_dates = [p['created_date'] for p in posts if p.get('created_date')]
+            oldest_post_date = min(post_dates)
+            today = datetime.today()
+            delta = relativedelta(today, oldest_post_date)
+            months = delta.years * 12 + delta.months + 1  
+            posting_frequency = round(posts_amount / months, 2)
+        except Exception as e:
+            print("Error calculating posting frequency:", e)
     return render(request, 'analysis.html', {
         "pic":pic,
         "date_today":date_today,
@@ -51,9 +82,14 @@ def analysispage(request):
         "post_karma":post_karma,
         "comment":comment,
         "comment_karma":comment_karma,
-        "subredits1":subredits[0],
-        "subredits2":subredits[1],
-        "subredits3":subredits[2],
-        "up_v_down":up_v_down/n,
+        "subredits1":subredits1,
+        "subredits2":subredits2,
+        "subredits3":subredits3,
+        "up": round(up/n,2),
+        "up_v_down":round(up_v_down/n,2),
+        "comment_amount":round(comment_amount/n,2),
+        "averal_comments":comment_amount,
+        "j":j,
+        "posting_frequency": posting_frequency,
         "query": query 
     })
