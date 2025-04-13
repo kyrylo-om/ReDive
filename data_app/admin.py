f01a1e8b2826bf7d0597fe678c67a9c86d993212
Luka Konovalov <konovluka@gmail.com>
2025-04-13 15:19:31 +0300

    Made models for Person to represent reddit user, History that have entries as analysis of user and are linked to Person by Foreignkey, and Post and Comment are linked to History. Added registration of those models to admin page to make work easier.

--- data_app/admin.py
+++ data_app/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+from .models import Person, History, Post, Comment
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('person', 'analyzed_at', 'karma', 'bot_likelihood_percent')
+    list_filter = ('analyzed_at',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('history', 'title', 'subreddit', 'created_date')
+    list_filter = ('created_date', 'subreddit')
+    search_fields = ('title', 'subreddit')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('history', 'subreddit', 'created_date')
+    list_filter = ('created_date', 'subreddit')
+    search_fields = ('subreddit',)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_analysis')
+    search_fields = ('name',)
+    list_filter = ('last_analysis',)
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(History, HistoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- data_app/models.py
+++ data_app/models.py
@@ -1,15 +1,68 @@
 from django.db import models
+from django.utils import timezone
 
-# class History(models.Model):
-#     name = models.TextField(max_length=75)
+class Person(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+    entry_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(null=True, blank=True)
+    last_analysis = models.ForeignKey('History', null=True, blank=True, on_delete=models.SET_NULL, related_name="last_analysis_for_person")
 
-#     def __str__(self):
-#         return f'{self.name}'
+    def __str__(self):
+        return f'{self.name}'
 
-# class Person(models.Model):
-#     history = models.ForeignKey(History, on_delete=models.CASCADE)
-#     name=models.TextField(max_length=75)
-#     date = models.TextField(max_length=75)
+    def update_last_analysis(self):
+        """Оновлює посилання на останній аналіз користувача"""
+        latest_history = self.histories.order_by('-analyzed_at').first()
+        self.last_analysis = latest_history
+        self.save()
 
-#     def __str__(self):
-#         return f"{self.name}"
+class History(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="histories")
+    analyzed_at = models.DateTimeField(default=timezone.now)
+    karma = models.IntegerField()
+    link_karma = models.IntegerField()
+    comment_karma = models.IntegerField()
+    is_mod = models.BooleanField()
+    is_employee = models.BooleanField()
+    trophies = models.JSONField(default=list)
+    is_gold = models.BooleanField()
+    verified = models.BooleanField()
+    has_verified_email = models.BooleanField()
+    bot_likelihood_percent = models.IntegerField()
+    subreddit = models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        analyzed_at = self.analyzed_at.strftime('%Y-%m-%d %H:%M')
+        return f"{self.person.name} @ {analyzed_at}"
+
+    def save(self, *args, **kwargs):
+
+        super().save(*args, **kwargs)
+        self.person.update_last_analysis()
+
+class Post(models.Model):
+    history = models.ForeignKey(History, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    subreddit = models.CharField(max_length=100)
+    permalink = models.URLField()
+    url = models.URLField()
+    score = models.IntegerField()
+    upvotes_ratio = models.FloatField()
+    created_date = models.DateField()
+    num_comments = models.IntegerField()
+    over_18 = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.url}"
+
+class Comment(models.Model):
+    history = models.ForeignKey(History, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    score = models.IntegerField()
+    subreddit = models.CharField(max_length=100)
+    url = models.URLField()
+    created_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.url}"
+
