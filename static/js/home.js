84d188a65a2365615e15bed26db3e6aece6b85e0
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2025-04-28 10:20:09 +0300

    Add an animation for user count

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,27 @@
+const live_users = document.getElementById("live_users");
+
+function animateNumber(finalNumber, duration = 1000, startNumber = 0, callback) {
+    const startTime = performance.now();
+    
+    function easeOutQuad(t) {
+        return 1 - Math.pow(1 - t, 3);
+    }
+    
+    function update(currentTime) {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        const easedProgress = easeOutQuad(progress);
+        const currentNumber = Math.floor(easedProgress * (finalNumber - startNumber) + startNumber);
+      
+        callback(currentNumber);
+        
+        if (progress < 1) {
+            requestAnimationFrame(update);
+        }
+    }
+    requestAnimationFrame(update);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const infoBtn = document.getElementById("info-button");
     if (infoBtn) {
@@ -12,5 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const infoButton = document.getElementById('info-button');
     const infoUrl = infoButton.dataset.infoUrl;
     infoButton.style.backgroundImage = `url('${infoUrl}')`;
-  });
+
+    animateNumber(386, 1000, 0, (n) => {
+        live_users.textContent = n;
+    });
+});
   
